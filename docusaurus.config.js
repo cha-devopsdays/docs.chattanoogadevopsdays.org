fe4241aa63b397d435b3a1d6b563f24d03fdcb32
Aaron Sachs <98627+asachs01@users.noreply.github.com>
2023-02-27 16:10:33 -0500

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  noIndex: true,
+  noIndex: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
